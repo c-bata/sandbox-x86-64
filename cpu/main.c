15e0796388535a98ca1da929c316d9183ca6319e
c-bata <contact@c-bata.link>
2022-01-06 15:26:26 +0900

    cpu: Add --format option

--- cpu/main.c
+++ cpu/main.c
@@ -11,6 +11,14 @@
 #define MEMORY_SIZE (1024 * 1024)
 
 bool quiet = false;
+
+enum formats {
+    BIN,      // Flat raw binary [default]
+    MACHO64,  // Mach-O x86-64 (Mach, including macOS variants)
+    // ELF64, // ELF64(x86-64) (Linux, most Unix variants)
+};
+int format = BIN;
+
 char* registers_name[] = {
         "RAX", "RCX", "RDX", "RBX", "RSP", "RBP", "RSI", "RDI",
         "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15"
@@ -76,21 +84,38 @@ int main(int argc, char* argv[]) {
         if (strcmp(argv[i], "-q") == 0 || strcmp(argv[i], "--quiet") == 0) {
             quiet = true;
             argc = opt_remove_at(argc, argv, i);
+        } else if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--format") == 0) {
+            argc = opt_remove_at(argc, argv, i);
+
+            if (i >= argc)
+                errorf("invalid --format option [bin, elf64, macho64]");
+
+            if (strcmp(argv[i], "bin") == 0)
+                format = BIN;
+            else if (strcmp(argv[i], "macho64") == 0)
+                format = MACHO64;
+            else
+                errorf("invalid --format option [bin, elf64, macho64]");
+            argc = opt_remove_at(argc, argv, i);
         } else {
             i++;
         }
     }
 
-    emu = create_emu(MEMORY_SIZE, 0x7c00, 0x7c00);
+    if (format == MACHO64) {
+        errorf("implemented me\n");
+    } else if (format == BIN) {
+        emu = create_emu(MEMORY_SIZE, 0x7c00, 0x7c00);
 
-    binary = fopen(argv[1], "rb");
-    if (binary == NULL) {
-        errorf("cannot open file '%s'\n", argv[1]);
+        binary = fopen(argv[1], "rb");
+        if (binary == NULL) {
+            errorf("cannot open file '%s'\n", argv[1]);
+        }
+        // read machine program (max 512 bytes)
+        fread(emu->memory + 0x7c00, 1, 0x200, binary);
+    } else {
+        errorf("unsupported format: %d", format);
     }
-
-    // read machine program (max 512 bytes)
-    fread(emu->memory + 0x7c00, 1, 0x200, binary);
-    fclose(binary);
 
     init_instructions();
 
@@ -115,5 +140,6 @@ int main(int argc, char* argv[]) {
 
     int exit_status = (int) emu->registers[RAX];
     destroy_emu(emu);
+    fclose(binary);
     return exit_status;
 }
