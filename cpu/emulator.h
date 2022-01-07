7b79424d73c460190ce2273be4efb723ce0ef990
c-bata <contact@c-bata.link>
2022-01-08 06:02:53 +0900

    cpu: Update register definisions

--- cpu/emulator.h
+++ cpu/emulator.h
@@ -9,7 +9,11 @@ enum Register {
     R8, R9, R10, R11, R12, R13, R14, R15,
     REGISTERS_COUNT,
     AL = RAX, CL = RCX, DL = RDX, BL = RBX,
-    AH = AL + 4, CH = CL + 4, DH = DL + 4, BH = BL + 4};
+    SPL = RSP, BPL = RBP, SIL = RSI, DIL = RDI,
+    R8B = R8, R9B = R9, R10B = R10, R11B = R11,
+    R12B = R12, R13B = R13, R14B = R14, R15B = R15,
+    AH = AL + 4, CH = CL + 4, DH = DL + 4, BH = BL + 4,
+    SPH = SPL + 4, BPH = BPL + 4, SIH = SIL + 4, DIH = DIL + 4};
 
 typedef struct {
     uint64_t registers[REGISTERS_COUNT];
